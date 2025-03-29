e65f070672e09429b748a7cc37d629e6442e8360
Roman Kudravcev <romankudravcev1@gmail.com>
2025-03-29 12:28:55 +0100

    change nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,10 +6,17 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxthub/core"],
+  ssr: false,
+  hub: {
+    database: true,
+  },
+
   css: ["~/assets/css/main.css", "bootstrap-icons/font/bootstrap-icons.css"],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   nitro: {
     preset: "cloudflare-pages",
     experimental: {
@@ -19,7 +26,7 @@ export default defineNuxtConfig({
       myDatabase: {
         connector: "cloudflare-d1",
         options: {
-          bindingName: "dbLink",
+          bindingName: "dog-rescue",
         },
       },
     },
